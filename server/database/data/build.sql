7831d187d601000b9a62e11220f46f7de9360c6f
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-02-01 21:47:50 +0200

    still working on the database but needs translation

--- server/database/data/build.sql
+++ server/database/data/build.sql
@@ -25,7 +25,7 @@ CREATE TABLE users (
 CREATE TABLE products (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  description VARCHAR(255),
+  description TEXT,
   img_url TEXT,
   category VARCHAR(255),
   rating FLOAT,
