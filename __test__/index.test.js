ca082ca2f1c6f670a065d0d6c07be236ea329b8a
mFlafiel <msf-2oo7@hotmail.com>
2021-02-01 14:30:02 +0200

    add jest configration to eslint relates #5

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,5 +1,5 @@
 describe('initial test', () => {
-    it('knows that 2 and 2 make 4', () => {
-      expect(2 + 2).toBe(4);
-    });
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
   });
+});
