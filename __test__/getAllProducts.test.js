42a0cfd9d41ed5cc52f47a90d89729424783c68e
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-02-02 22:09:09 +0200

    testing the route and the database query Relate #8

---
+++ __test__/getAllProducts.test.js
@@ -0,0 +1,44 @@
+const request = require('supertest');
+const app = require('../server/app');
+const connection = require('../server/database/connection');
+const buildDb = require('../server/database/data/build');
+const { getAllProducts } = require('../server/database/quieres');
+
+// test('/products - GET ALL PRODUCTS', (done) => request(app)
+//   .get('/api/v1/products')
+//   .expect(200)
+//   .expect('Content-Type', /json/)
+//   .end((err, res) => {
+//     if (err) {
+//       return done(err);
+//     }
+//     expect(1).toBe(1);
+//     done();
+//   }));
+
+describe('Test Routes, Test Database getClients query', () => {
+  beforeEach(() => buildDb());
+  afterAll(() => connection.end());
+
+  test('getAllProducts >> This test will return clients data', () => getAllProducts()
+    .then((result) => {
+      const actual = result.rows[0];
+      const expected = {
+        id: 1, img_url: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg', name: 'حقيبة لابتوب 15 بوصة', new_price: 109.95, old_price: null, rating: 3.2366492489753718,
+      };
+      expect(actual).toEqual(expected);
+    })
+    .catch());
+  // eslint-disable-next-line jest/no-done-callback
+  test('Route GET /api/v1/products', (done) => request(app)
+    .get('/api/v1/products')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) return done(err);
+      expect(JSON.parse(res.text).data.length).toBe(20);
+      expect(JSON.parse(res.text).data[0].name).toBe('حقيبة لابتوب 15 بوصة');
+      expect(res.status).toBe(200);
+      return done();
+    }));
+});
