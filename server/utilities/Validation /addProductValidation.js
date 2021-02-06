daef3dacb2961474e63a86a894a487c361d9c6dd
mFlafiel <msf-2oo7@hotmail.com>
2021-02-06 18:09:06 +0200

    update validation schema to include a set of valid data for category relates #11

--- server/utilities/Validation /addProductValidation.js
+++ server/utilities/Validation /addProductValidation.js
@@ -1,20 +1,36 @@
 const Joi = require('joi');
 
 const addProductValidation = ({
-  name, description, img_url, category, rating, price, quantity, created_at,
+  name,
+  description,
+  img_url,
+  category,
+  rating,
+  price,
+  quantity,
+  created_at,
 }) => {
   const schema = Joi.object({
     name: Joi.string().required(),
     description: Joi.string().required(),
     img_url: Joi.string().required(),
-    category: Joi.string().required(),
+    category: Joi.string()
+      .valid('fashion', 'accessories', 'electronics', 'health')
+      .required(),
     rating: Joi.number().max(5),
     price: Joi.number().min(1).required(),
     quantity: Joi.number().min(1).required(),
     created_at: Joi.date().timestamp(),
   });
   const result = schema.validateAsync({
-    name, description, img_url, category, rating, price, quantity, created_at,
+    name,
+    description,
+    img_url,
+    category,
+    rating,
+    price,
+    quantity,
+    created_at,
   });
   return result;
 };
