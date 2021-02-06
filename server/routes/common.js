c9e2ec113e431cd9f2b31ccc8bd834272e713ae5
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-02-06 17:33:58 +0200

    fixing the issues Relates #26 #24 #51

--- server/routes/common.js
+++ server/routes/common.js
@@ -16,7 +16,6 @@ commonRouter.get('/products/top-rated', productsByTopRatingHandler);
 commonRouter.get('/products/trending', productsByTrendingHandler);
 commonRouter.get('/products/:category', productsByCategoryHandler);
 commonRouter.get('/product-search', productSearchCategoryHandler);
-commonRouter.get('/products', allProductsHandler);
 commonRouter.get('/product/:productId', productByIdHandler);
 
 module.exports = commonRouter;
