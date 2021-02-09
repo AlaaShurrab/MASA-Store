77cb2c73048c5fcd5093d68065033102f2f7d471
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-02-09 13:56:16 +0200

    adding proptypes to the selint and fixing the issues Relates #71

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function App() {
-  return <div>Welcome to MASA STORE</div>
+  return <div>Welcome to MASA STORE</div>;
 }
 
-export default App
+export default App;
