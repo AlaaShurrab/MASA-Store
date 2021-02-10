305ce73d2b6d2377352c70ccbf3edc59dc491980
Sondos-Alalami <sondos.alalami@gmail.com>
2021-02-10 23:57:22 +0200

    craeting inputField component with three sizes relates #86

--- client/src/component/InputField/InputField.js
+++ client/src/component/InputField/InputField.js
@@ -1,5 +1,66 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import EditIcon from '@material-ui/icons/Edit';
 import InputAdornment from '@material-ui/core/InputAdornment';
+
+const useStyles = makeStyles({
+  large: {
+    width: '96%',
+  },
+  small: {
+    width: '46%',
+  },
+  medium: {
+    width: '68%',
+  },
+});
+
+function InputField(props) {
+  const classes = useStyles();
+  const { size, placeholder, label, id } = props;
+
+  return (
+    <TextField
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <EditIcon style={{ color: '#949494' }} />
+          </InputAdornment>
+        ),
+      }}
+      id={id}
+      label={label}
+      placeholder={placeholder}
+      style={{ margin: 8 }}
+      margin="normal"
+      InputLabelProps={{
+        shrink: true,
+      }}
+      variant="outlined"
+      className={
+        size === 'small'
+          ? classes.small
+          : size === 'medium'
+          ? classes.medium
+          : classes.large
+      }
+    />
+  );
+}
+
+InputField.propTypes = {
+  size: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  placeholder: PropTypes.string,
+  label: PropTypes.string,
+};
+
+InputField.defaultProps = {
+  id: 'edit',
+  label: '',
+  placeholder: '',
+};
+
+export default InputField;
