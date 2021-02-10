50d8f71b7c428a2511a7e69f0516e7ceebf0712e
Sondos-Alalami <sondos.alalami@gmail.com>
2021-02-10 17:19:06 +0200

    craeting inputField component folder

---
+++ client/src/component/InputField/InputField.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import EditIcon from '@material-ui/icons/Edit';
+import InputAdornment from '@material-ui/core/InputAdornment';
