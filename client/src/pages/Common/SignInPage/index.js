bc56a8632647e0854419fc5f9f56a362c8fde010
Ahmed-aed <aeid767@gmail.com>
2021-02-14 17:51:13 +0200

    sign in page design related #77

--- client/src/pages/Common/SignInPage/index.js
+++ client/src/pages/Common/SignInPage/index.js
@@ -1,12 +1,116 @@
 import { Helmet } from 'react-helmet';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
 
-const SignInPage = () => (
-  <>
-    <Helmet>
-      <title>signin</title>
-    </Helmet>
-    <p>Welcome to SignInPage </p>
-  </>
-);
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import logo from '../../../assets/sign-in.svg';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        MASA
+      </Link>
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const SignInPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs" className="container">
+      <Grid>
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            تسجيل الدخول
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="البريد الﻹلكتروني"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="كلمة المرور"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              تسجيل الدخول
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link to="/sign-up" variant="body2">
+                  ليس لديك حساب ؟ سجل الآن
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Grid>
+      <Grid>
+        <img alt="logo" src={logo} />
+      </Grid>
+    </Container>
+  );
+};
 
 export default SignInPage;
