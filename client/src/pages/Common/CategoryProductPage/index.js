de79509d6863bf24ac101956d5c365a030af40e5
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-02-16 01:21:54 +0200

    creating the category page Relates #74

--- client/src/pages/Common/CategoryProductPage/index.js
+++ client/src/pages/Common/CategoryProductPage/index.js
@@ -1,6 +1,11 @@
+import { useEffect, useState } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import { Redirect, useParams } from 'react-router-dom';
+import axios from 'axios';
+
+import { CardContainer } from '../../../component';
+import dataFormatter from '../../../utilities/dataFormatter';
 
 const appCategory = [
   'fashion',
@@ -10,17 +15,38 @@ const appCategory = [
   'trending',
   'top-rated',
 ];
-const CategoryProductPage = ({ role }) => {
+const CategoryProductPage = ({ role, userData }) => {
+  const [data, setData] = useState([]);
   const { category } = useParams();
+
+  const dataCollector = async () => {
+    if (userData) {
+      const { favoriteData, cartProducts } = userData;
+      if (favoriteData && cartProducts) {
+        const {
+          data: { data: raring },
+        } = await axios(`/api/v1/products/${category}`, {});
+        setData(dataFormatter(raring, favoriteData, cartProducts));
+      }
+    }
+  };
+
+  useEffect(() => {
+    dataCollector();
+  }, [userData]);
+
   if (appCategory.includes(category)) {
     return (
       <>
         <Helmet>
           <title>category</title>
         </Helmet>
-        <p>
-          Hi {role},Welcome to product {category} page
-        </p>
+        <CardContainer
+          userData={userData}
+          role={role}
+          data={data}
+          pageTitle={category}
+        />
       </>
     );
   }
@@ -29,6 +55,13 @@ const CategoryProductPage = ({ role }) => {
 
 CategoryProductPage.propTypes = {
   role: PropTypes.string.isRequired,
+  userData: PropTypes.shape({
+    favoriteData: PropTypes.instanceOf(Array),
+    cartProducts: PropTypes.instanceOf(Array),
+    profileData: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
 };
 
 export default CategoryProductPage;
