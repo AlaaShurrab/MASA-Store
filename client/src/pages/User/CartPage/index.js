a4bf6ddb52740d5b72d35ba5a8dfffe6e343cf57
Sondos-Alalami <sondos.alalami@gmail.com>
2021-02-16 05:43:44 +0200

    handling isFavorite property for the products in cart relates #85

--- client/src/pages/User/CartPage/index.js
+++ client/src/pages/User/CartPage/index.js
@@ -1,9 +1,12 @@
+import { React, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { Typography } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 import CartCard from '../../../component/Card/index';
 
 const useStyles = makeStyles((theme) => ({
@@ -12,15 +15,40 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
+    marginTop: theme.spacing(5),
+    boxSizing: 'border-box',
     textAlign: 'center',
     color: theme.palette.text.secondary,
+    // marginRight: theme.spacing(5),
+  },
+  cartProductsContainer: {
+    marginTop: theme.spacing(3),
+
+    // marginLeft: theme.spacing(5),
+    boxSizing: 'border-box',
+  },
+  cartProduct: {
+    marginTop: theme.spacing(2),
   },
 }));
 const CartPage = (props) => {
   const classes = useStyles();
+  // const [isFavorite, setIsFavorite] = useState();
+  const [favoriteProducts, setFavoriteProducts] = useState([]);
+  const { cartProducts, favoriteData } = props.userData;
 
-  const { cartProducts } = props.userData;
   console.log(cartProducts);
+  let totalPrice = 0;
+  let fa = [];
+  cartProducts.map((product) => {
+    totalPrice += product.new_price;
+  });
+
+  favoriteData.length !== 0 || cartProducts.length !== 0
+    ? (fa = favoriteData.map((product) => product.product_id))
+    : (fa = null);
+
+  useEffect(() => {}, []);
 
   return (
     <>
@@ -28,29 +56,64 @@ const CartPage = (props) => {
         <title>cart</title>
       </Helmet>
       <div className={classes.root}>
-        <Grid container spacing={2}>
-          <Grid item xs={8} sm={6}>
-            {cartProducts.map((product) => (
-              <CartCard data={product} />
-            ))}
+        <Grid container justify="space-around">
+          <Grid item sm={7} className={classes.cartProductsContainer}>
+            {cartProducts.map((product) => {
+              const isFavorite = fa.includes(product.product_id);
+              return (
+                <div className={classes.cartProduct}>
+                  <CartCard data={product} isFavorite={isFavorite} />
+                </div>
+              );
+            })}
           </Grid>
-          <Grid item xs={4} sm={6}>
-            <Paper className={classes.paper}>
+          <Grid item sm={3}>
+            <Paper className={classes.paper} elevation={3}>
               <Grid container spacing={4}>
                 <Grid item xs={12}>
-                  <Typography>ملخص الطلبية</Typography>
+                  <Typography
+                    color="textPrimary"
+                    variant="h6"
+                    style={{ fontWeight: 'bold', textAlign: 'start' }}
+                  >
+                    ملخص الطلبية
+                  </Typography>
                 </Grid>
                 <Grid item xs={6}>
-                  المجموع الجزئي
+                  <Typography> المجموع الجزئي</Typography>
                 </Grid>
                 <Grid item xs={6}>
-                  12
+                  <Typography>₪ {totalPrice}</Typography>
                 </Grid>
                 <Grid item xs={6}>
-                  تكلفة الشحن
+                  <Typography> تكلفة الشحن</Typography>
                 </Grid>
                 <Grid item xs={6}>
-                  12
+                  <Typography>₪ 12</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Divider />
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography
+                    color="textPrimary"
+                    style={{ fontWeight: 'bold' }}
+                  >
+                    الإجمالي
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography
+                    color="textPrimary"
+                    style={{ fontWeight: 'bold' }}
+                  >
+                    ₪ 12
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button variant="contained" color="primary">
+                    شراء
+                  </Button>
                 </Grid>
               </Grid>
             </Paper>
