1ff8e14d6b80788418652e0dd11128c026a13a94
Ahmed-aed <aeid767@gmail.com>
2021-02-17 17:38:42 +0200

    profile page update fixes #80

--- client/src/pages/User/ProfilePage/index.js
+++ client/src/pages/User/ProfilePage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import AccountBoxRoundedIcon from '@material-ui/icons/AccountBoxRounded';
 import Grid from '@material-ui/core/Grid';
@@ -10,10 +10,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Container from '@material-ui/core/Container';
 import Skeleton from '@material-ui/lab/Skeleton';
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import { ButtonComponent } from '../../../component';
-
-import profile from '../../../assets/profile.svg';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -52,7 +51,53 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const ProfilePage = ({ userData }) => {
+  const [state, setState] = useState({
+    first_name: '',
+    last_name: '',
+    avatar: '',
+    payment_card_name: '',
+    payment_card_number: 0,
+    payment_card_expire_date: 0,
+    city: '',
+    shipping_address1: '',
+    shipping_address2: '',
+  });
   useEffect(() => {}, [userData.profileData]);
+
+  const handleChangInput = (e, type) => {
+    setState({
+      ...state,
+      [type]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const {
+      first_name,
+      last_name,
+      avatar,
+      payment_card_name,
+      payment_card_number,
+      payment_card_expire_date,
+      city,
+      shipping_address1,
+      shipping_address2,
+    } = state;
+    axios
+      .put(`api/v1//profile/${userData.profileData.id}`, {
+        firstName: first_name,
+        lastName: last_name,
+        avatar,
+        shippingAddress1: shipping_address1,
+        shippingAddress2: shipping_address2,
+        city,
+        paymentCardName: payment_card_name,
+        paymentCardNumber: payment_card_number,
+        paymentCardExpireDate: payment_card_expire_date,
+      })
+      .then(() => alert('تم تحدث البيانات بنجاح '));
+  };
+
   const classes = useStyles();
   return (
     <>
@@ -84,22 +129,24 @@ const ProfilePage = ({ userData }) => {
                 <Grid item xs={12} sm={6} md={4}>
                   <TextField
                     defaultValue={userData.profileData.first_name}
-                    name="firstName"
+                    name="first_name"
                     variant="outlined"
                     label="الأسم الأول "
                     type="text"
                     fullWidth
+                    onChange={(e) => handleChangInput(e, 'first_name')}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                   <TextField
-                    name="lastName"
+                    name="last_name"
                     variant="outlined"
                     defaultValue={userData.profileData.last_name}
                     required
                     label="الأسم الثاني "
                     type="text"
                     fullWidth
+                    onChange={(e) => handleChangInput(e, 'last_name')}
                   />
                 </Grid>
               </Grid>
@@ -110,8 +157,9 @@ const ProfilePage = ({ userData }) => {
                     defaultValue={userData.profileData.avatar}
                     required
                     label="رابط الصورة الشخصية "
-                    name="الصورة الشخصية "
+                    name="avatar"
                     fullWidth
+                    onChange={(e) => handleChangInput(e, 'avatar')}
                   />
                   <div className={classes.emailDiv}>
                     <Typography className={classes.mtb20} variant="h6">
@@ -124,7 +172,11 @@ const ProfilePage = ({ userData }) => {
                   <div className={classes.profilePhoto}>
                     <Avatar
                       alt="profile"
-                      src={profile}
+                      src={
+                        state.avatar
+                          ? state.avatar
+                          : userData.profileData.avatar
+                      }
                       className={classes.large}
                     />
                   </div>
@@ -141,32 +193,40 @@ const ProfilePage = ({ userData }) => {
               <Grid container spacing={3}>
                 <Grid item xs={12} sm={6} md={8}>
                   <TextField
+                    name="payment_card_name"
                     variant="outlined"
                     required
                     id="cardName"
                     defaultValue={userData.profileData.payment_card_name}
                     label="الاسم علي البطاقة"
                     fullWidth
+                    onChange={(e) => handleChangInput(e, 'payment_card_name')}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6} md={8}>
                   <TextField
                     variant="outlined"
+                    name="payment_card_number"
                     required
                     id="cardNumber"
                     defaultValue={userData.profileData.payment_card_number}
                     label="رقم البطاقة "
                     fullWidth
+                    onChange={(e) => handleChangInput(e, 'payment_card_number')}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6} md={8}>
                   <TextField
                     variant="outlined"
                     required
+                    name="payment_card_expire_date"
                     id="expDate"
                     defaultValue={userData.profileData.payment_card_expire_date}
                     label="تاريخ الإنتهاء"
                     fullWidth
+                    onChange={(e) =>
+                      handleChangInput(e, 'payment_card_expire_date')
+                    }
                   />
                 </Grid>
               </Grid>
@@ -181,32 +241,38 @@ const ProfilePage = ({ userData }) => {
               <Grid container spacing={3}>
                 <Grid item xs={12} sm={6} md={8}>
                   <TextField
+                    name="city"
                     variant="outlined"
                     required
                     defaultValue={userData.profileData.city}
                     id="city"
                     label="المدينة"
                     fullWidth
+                    onChange={(e) => handleChangInput(e, 'city')}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6} md={8}>
                   <TextField
+                    name="shipping_address1"
                     variant="outlined"
                     required
                     defaultValue={userData.profileData.shipping_address1}
                     id="shipping_address1"
                     label="العنوان الأول"
                     fullWidth
+                    onChange={(e) => handleChangInput(e, 'shipping_address1')}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6} md={8}>
                   <TextField
+                    name="shipping_address2"
                     variant="outlined"
                     required
                     defaultValue={userData.profileData.shipping_address2}
                     id="shipping_address2"
                     label="العنوان الثاني "
                     fullWidth
+                    onChange={(e) => handleChangInput(e, 'shipping_address2')}
                   />
                 </Grid>
               </Grid>
@@ -215,6 +281,7 @@ const ProfilePage = ({ userData }) => {
               className={classes.mtb20}
               text="تحديث"
               color="primary"
+              onClick={handleSubmit}
             />
           </Container>
         </>
@@ -230,6 +297,7 @@ const ProfilePage = ({ userData }) => {
     </>
   );
 };
+
 ProfilePage.propTypes = {
   userData: PropTypes.shape({
     profileData: PropTypes.shape({
@@ -238,11 +306,12 @@ ProfilePage.propTypes = {
       avatar: PropTypes.string,
       email: PropTypes.string,
       payment_card_name: PropTypes.string,
-      payment_card_number: PropTypes.string,
-      payment_card_expire_date: PropTypes.string,
+      payment_card_number: PropTypes.number,
+      payment_card_expire_date: PropTypes.number,
       city: PropTypes.string,
       shipping_address1: PropTypes.string,
       shipping_address2: PropTypes.string,
+      id: PropTypes.string,
     }),
   }).isRequired,
 };
