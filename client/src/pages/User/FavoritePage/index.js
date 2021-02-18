5dc86dae114ac10b87e22ae8b29d99392be05b9d
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-02-18 12:50:07 +0200

    creating and displaying the data in the page

--- client/src/pages/User/FavoritePage/index.js
+++ client/src/pages/User/FavoritePage/index.js
@@ -1,16 +1,52 @@
+import { useEffect, useState } from 'react';
+// import { Redirect, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
+import axios from 'axios';
 
-const FavoritePage = ({ type }) => (
-  <>
-    <Helmet>
-      <title>Favorite</title>
-    </Helmet>
-    <p>Hi {type},Welcome to FavoritePage </p>
-  </>
-);
+import { CardContainer } from '../../../component';
+import dataFormatter from '../../../utilities/dataFormatter';
+
+const FavoritePage = ({ type, userData }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const dataCollector = async () => {
+      if (userData) {
+        const { favoriteIds, cartIds } = userData;
+        if (favoriteIds && cartIds) {
+          const {
+            data: { data: favorite },
+          } = await axios(`/api/v1/favorite/${userData.profileData.id}`, {});
+          // console.log(favorite);
+          setData(dataFormatter(favorite, favoriteIds, cartIds));
+        }
+      }
+    };
+    dataCollector();
+  }, [userData]);
+
+  // console.log(data);
+  return (
+    <>
+      <Helmet>
+        <title>Favorite</title>
+      </Helmet>
+      <p>Hi {type},Welcome to FavoritePage </p>
+      <CardContainer
+        userData={userData}
+        role={type}
+        data={data}
+        pageTitle="favoriteIds"
+      />
+    </>
+  );
+};
 
 FavoritePage.propTypes = {
   type: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  userData: PropTypes.object.isRequired,
 };
+
 export default FavoritePage;
--- client/src/utilities/dataFormatter.js
+++ client/src/utilities/dataFormatter.js
@@ -1,15 +1,20 @@
-const dataFormatter = (res, favoriteData, cartProducts) =>
+const dataFormatter = (res, favoriteData, cartProducts, favorite = false) =>
   res.map((item) => {
     let isCart = false;
     let isFavorite = false;
+    let id;
     if (favoriteData.length !== 0 || cartProducts.length !== 0) {
-      isCart = cartProducts.includes(item.id);
-      isFavorite = favoriteData.includes(item.id);
+      isCart = cartProducts.includes(favorite ? item.product_id : item.id);
+      isFavorite = favoriteData.includes(favorite ? item.product_id : item.id);
     }
+    // if (favorite) {
+    //   id = item.product_id;
+    // }
     return {
       ...item,
       isFavorite,
       isCart,
+      id,
     };
   });
 
